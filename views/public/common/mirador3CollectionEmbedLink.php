10a318978f131c4ea48cb4f986862cefc775b747
Jeremy Guillette <jguillette@fas.harvard.edu>
2020-12-01 10:34:05 -0500

    fix url in embed code

--- views/public/common/mirador3CollectionEmbedLink.php
+++ views/public/common/mirador3CollectionEmbedLink.php
@@ -1,7 +1,7 @@
 <div aria-live="polite">
   <h3><?php echo link_to_collection(__('Mirador 3 viewer'), array('id' => 'mirador-3-embed-link'), 'mirador3', $collection); ?></h3>
   <label for="mirador-3-embed-code">Embed Code: </label>
-  <input style="display:inline-block;" type="text" name="mirador-3-embed-code" id="mirador-3-embed-code" value='<iframe src="<?php echo absolute_url(array("things" => "collection", "id" => $collection->id), 'iiifItemEmbed_Mirador3'); ?>" width="100%" height="600px"></iframe>'>
+  <input style="display:inline-block;" type="text" name="mirador-3-embed-code" id="mirador-3-embed-code" value='<iframe src="<?php echo absolute_url(array("things" => "collections", "id" => $collection->id), 'iiifItemEmbed_Mirador3'); ?>" width="100%" height="600px"></iframe>'>
   <button type="button" name="button" onclick="copyIiifEmbedCode('mirador-3-embed-code')">Copy embed code</button>
   <span id="mirador-3-embed-code-update"></span>
   <script type="text/javascript">
